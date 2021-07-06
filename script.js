a95db114d4a62e664d7ff31cbc70cb5431ef8c46
Michelle Ly <lymichelle2016@icloud.com>
2021-07-06 19:26:28 -0400

    Initial Sound Works

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-var button = $('.secret-btn'); 
+var button = $('.play-btn');
+var audio = new Audio("Keys/C4.ogg");
 
-button.on("click", play); 
+button.on("click", play);
 
 function play(){
-  var i = document.getElementById("img1"); 
-  console.log(i.src)
-    if (i.src === "Piano Bear Idle.png") {
-      i.src = "Piano Bear Play.png";
-    }
-    else {
-      i.src = "Piano Bear Idle.png";
-    }
 }
+
+function playAudio(fileName) {
+  var audio = new Audio(fileName);
+  audio.currentTime=0;
+  audio.play();
+  setInterval(function(){
+    if (audio.currentTime>2.5)
+      audio.pause();
+  })
+}
+
+document.addEventListener('keydown', function(e){
+
+  var i = document.getElementById("img1");
+  if (i.src.includes("Piano%20Bear%20Idle.png"))
+    i.src = "Piano Bear Play.png";
+  else
+    i.src = "Piano Bear Idle.png";
+
+  if(e.keyCode==65)     // A
+    playAudio("Keys/C4.ogg");
+  else if(e.keyCode==83)   // S
+    playAudio("Keys/D3.ogg");
+});
